983ec55e0d7f7df28814789a90508ea1343d7cf4
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-12-04 18:14:17 +0300

    Add tabs

--- app/src/main/java/com/example/kirill/cookbook/MainActivity.java
+++ app/src/main/java/com/example/kirill/cookbook/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.kirill.cookbook;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ViewPager pager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,51 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
+
+        // Connect tabLayout with viewPager
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public static final int SECTIONS_COUNT = 3;
+
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new CategoriesFragment();
+                case 1:
+                    return new FavoriteFragment();
+                case 2:
+                    return new StoreFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return SECTIONS_COUNT;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.categories_tab);
+                case 1:
+                    return getResources().getText(R.string.favorite_tab);
+                case 2:
+                    return getResources().getText(R.string.shop_tab);
+            }
+            return null;
+        }
     }
 }
+
