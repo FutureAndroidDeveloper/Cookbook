7e850cddecccadf44a2334c2255a63e0b7b7e123
Kirill K <dev.kirill.klimenkov@gmail.com>
2018-12-16 21:13:20 +0300

    Select food from db at category fragment

--- app/src/main/java/com/example/kirill/cookbook/CategoriesFragment.java
+++ app/src/main/java/com/example/kirill/cookbook/CategoriesFragment.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +23,15 @@ import java.util.Random;
  * A simple {@link Fragment} subclass.
  */
 public class CategoriesFragment extends Fragment {
-    public String test;
+    public final String[] DatabaseCategories= {"Snacks", "Salads", "Main", "Desserts"};
+
+    public static final int gridColumnCount = 2;
+    public int[][] categoriesIds;
+    public int randomId;
     private final Random random = new Random();
+
+    public int[][] images;
+    public String[][] names;
 
     public CategoriesFragment() {
         // Required empty public constructor
@@ -36,82 +44,56 @@ public class CategoriesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_categories, container, false);
 
+        if (savedInstanceState != null) {
+            names[0] = savedInstanceState.getStringArray("SnackNames");
+            names[1] = savedInstanceState.getStringArray("SaladNames");
+            names[2] = savedInstanceState.getStringArray("MainNames");
+            names[3] = savedInstanceState.getStringArray("DessertNames");
+
+            images[0] = savedInstanceState.getIntArray("SnackImages");
+            images[1] = savedInstanceState.getIntArray("SaladImages");
+            images[2] = savedInstanceState.getIntArray("MainImages");
+            images[3] = savedInstanceState.getIntArray("DessertImages");
+        }
+
+//        categoriesIds = new int[DatabaseCategories.length][gridColumnCount];
+//        images = new int[DatabaseCategories.length][gridColumnCount];
+//        names = new String[DatabaseCategories.length][gridColumnCount];
+
         // Random choice of food from BD by CATEGORY
         FoodDatabaseHelper foodDatabaseHelper = new FoodDatabaseHelper(inflater.getContext());
         try {
             SQLiteDatabase database = foodDatabaseHelper.getReadableDatabase();
-            Cursor cursor = database.query("FOOD",
-                    new String[]{"_id", "CATEGORY", "NAME", "INGREDIENTS", "RECIPE", "IMAGE_RESOURCE_ID"},
-                    "CATEGORY = ?",
-                    new String[]{"Snacks"},
-                    null,
-                    null,
-                   null,
-                    null);
 
-//            int firstId = getFirstIdCursor(cursor);
-//            int cursorSize = getCursorSize(cursor);
-//            int lastId = (firstId + cursorSize) - 1;
-//            cursor.close();
+            for (int rowGrid = 0; rowGrid < DatabaseCategories.length; rowGrid++) {
+                for (int columnGrid = 0; columnGrid < gridColumnCount; columnGrid++) {
+                    Cursor cursor = getRandomCategoryRecordFromDb(database, DatabaseCategories[rowGrid]);
 
-            // Скорее всего нумерация срок в курсоре начинается с 1
-            int firstId = getFirstIdCursor(cursor); //1     5
-            int cursorSize = getCursorSize(cursor); //3     5
-            int lastId = (firstId + cursorSize);    //4     10
-            cursor.close();
-
-            // Random first food
-            int randomBetweenId = random.nextInt(lastId - firstId) + firstId;  // (4 - 1) = 3 --> [0; 2]
-                                                                                            // 0+1; 1+1; 2+1; --> 1;2;3
-                                                                                            // (10 - 5) = 5 --> [0; 4]
-                                                                                            // 0+5; 1+5; 2+5; 3+5; 4+5; --> 5;6;7;8;9
-
-            // Add second random with checking, that first random and second random are not equals!
-            // Use while()
-
-            cursor = database.query("FOOD",
-                    new String[]{"_id", "CATEGORY", "NAME", "INGREDIENTS", "RECIPE", "IMAGE_RESOURCE_ID"},
-                    "_id = ?",
-                    new String[]{Integer.toString(randomBetweenId)},
-                    null,
-                    null,
-                    null,
-                    null);
-
-            if (cursor.moveToFirst()) {
-                // g
-                test = cursor.getString(cursor.getColumnIndex("NAME"));
+                    // get all needed information about food
+                    if (cursor.moveToFirst()) {
+                        categoriesIds[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("_id"));
+                        images[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("IMAGE_RESOURCE_ID"));
+                        names[rowGrid][columnGrid] = cursor.getString(cursor.getColumnIndex("NAME"));
+                    }
+                }
             }
 
-            // get from cursor second random food here
-
-            cursor.close();
         } catch (SQLException e) {
             Toast.makeText(inflater.getContext(), "Database unavailable", Toast.LENGTH_SHORT).show();
         }
 
 
-        // Probable better will be add support class for storing info about food
-        // Or add information about food in arrays
-        int[] images = new int[Food.foods.length];
-        String[] names = new String[Food.foods.length];
-
-        for (int i = 0; i < Food.foods.length; i++) {
-            names[i] = Food.foods[i].getName();
-            images[i] = Food.foods[i].getImageResourceId();
-        }
-
         GridView gridViewSnack = (GridView) view.findViewById(R.id.gridViewSnack);
-        gridViewSnack.setAdapter(new CardGridAdapter(this.getContext(), names, images, R.layout.card_captioned_image));
+        gridViewSnack.setAdapter(new CardGridAdapter(this.getContext(), names[0], images[0], R.layout.card_captioned_image));
 
         GridView gridViewSalads = (GridView) view.findViewById(R.id.gridViewSalads);
-        gridViewSalads.setAdapter(new CardGridAdapter(this.getContext(), names, images, R.layout.card_captioned_image));
+        gridViewSalads.setAdapter(new CardGridAdapter(this.getContext(), names[1], images[1], R.layout.card_captioned_image));
 
         GridView gridViewDishes = (GridView) view.findViewById(R.id.gridViewDishes);
-        gridViewDishes.setAdapter(new CardGridAdapter(this.getContext(), names, images, R.layout.card_captioned_image));
+        gridViewDishes.setAdapter(new CardGridAdapter(this.getContext(), names[2], images[2], R.layout.card_captioned_image));
 
         GridView gridViewDesserts = (GridView) view.findViewById(R.id.gridViewDesserts);
-        gridViewDesserts.setAdapter(new CardGridAdapter(this.getContext(), names, images, R.layout.card_captioned_image));
+        gridViewDesserts.setAdapter(new CardGridAdapter(this.getContext(), names[3], images[3], R.layout.card_captioned_image));
 
         gridViewSnack.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -126,21 +108,6 @@ public class CategoriesFragment extends Fragment {
         return view;
     }
 
-    private int getCursorSize(Cursor cursor) {
-        int size = 0;
-
-        if (cursor.moveToFirst()) {
-            size++;
-        }
-
-        while (cursor.moveToNext()) {
-            size++;
-        }
-
-        cursor.moveToFirst();
-        return size;
-    }
-
     private int getFirstIdCursor(Cursor cursor) {
         int id = 0;
 
@@ -149,5 +116,52 @@ public class CategoriesFragment extends Fragment {
         }
 
         return id;
+    }
+
+    private Cursor getRandomCategoryRecordFromDb(SQLiteDatabase database, String category) {
+        Cursor cursor = database.query("FOOD",
+                new String[]{"_id", "CATEGORY", "NAME", "INGREDIENTS", "RECIPE", "IMAGE_RESOURCE_ID"},
+                "CATEGORY = ?",
+                new String[]{category},
+                null,
+                null,
+                null,
+                null);
+
+        int firstId = getFirstIdCursor(cursor);
+        int cursorSize = cursor.getCount();
+        int lastId = (firstId + cursorSize);
+
+        // Random record food
+        int randomBetweenId = random.nextInt(lastId - firstId) + firstId;
+
+        while (randomBetweenId == randomId) {
+            randomBetweenId = random.nextInt(lastId - firstId) + firstId;
+        }
+        randomId = randomBetweenId;
+
+        cursor = database.query("FOOD",
+                new String[]{"_id", "CATEGORY", "NAME", "INGREDIENTS", "RECIPE", "IMAGE_RESOURCE_ID"},
+                "_id = ?",
+                new String[]{Integer.toString(randomBetweenId)},
+                null,
+                null,
+                null,
+                null);
+
+        return  cursor;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putStringArray("SnackNames", names[0]);
+        outState.putStringArray("SaladNames", names[1]);
+        outState.putStringArray("MainNames", names[2]);
+        outState.putStringArray("DessertNames", names[3]);
+
+        outState.putIntArray("SnackImages", images[0]);
+        outState.putIntArray("SaladImages", images[1]);
+        outState.putIntArray("MainImages", images[2]);
+        outState.putIntArray("DessertImages", images[3]);
     }
 }
--- app/src/main/java/com/example/kirill/cookbook/FoodDatabaseHelper.java
+++ app/src/main/java/com/example/kirill/cookbook/FoodDatabaseHelper.java
@@ -52,6 +52,38 @@ public class FoodDatabaseHelper extends SQLiteOpenHelper {
                     "1. Купить;2. Высушить;3. Продать", R.drawable.pasta);
             insertFood(database, "Snacks", "Pizza", "1. Влад Пушко;2. Дмитрий Костеж;3. Дмитрий Чириков",
                     "1. Украсть;2. Отмыть;3. Скурить", R.drawable.pizza);
+            insertFood(database, "Snacks", "Soup", "1. Самый;2. Ненавидимый;3. Мц",
+                    "1. Вылечу;2. Вас;3. От", R.drawable.zamay);
+
+
+            insertFood(database, "Salads", "Жак", "1. В;2. легких;3. снова",
+                    "1. дым;2. я;3. не могу", R.drawable.a);
+            insertFood(database, "Salads", "Энтони", "1. вдохнуть;2. воздух;3. но мне",
+                    "1. плевать;2. я;3. бездыханным", R.drawable.b);
+            insertFood(database, "Salads", "Окси", "1. останусь;2. если;3. так",
+                    "1. надо;2. бездыханным;3. останусь", R.drawable.c);
+            insertFood(database, "Salads", "Рики", "1. не;2. говори;3. что",
+                    "1. я;2. не;3. могу", R.drawable.d);
+
+
+            insertFood(database, "Main", "баста", "1. я;2. долго;3. искал",
+                    "1. свой;2. путь;3. стиль", R.drawable.e);
+            insertFood(database, "Main", "каста", "1. потратил;2. тут;3. столько",
+                    "1. сил;2. чтобы;3. мой", R.drawable.f);
+            insertFood(database, "Main", "гнойный", "1. мир;2. меня;3. простил",
+                    "1. я;2. свеж;3. и", R.drawable.g);
+            insertFood(database, "Main", "мармиладный", "1. молод;2. пока;3. я",
+                    "1. продал;2. дух;3. с молотка", R.drawable.h);
+
+
+            insertFood(database, "Desserts", "ЩУР", "1. Где же;2. начало;3. пути",
+                    "1. найти;2. я;3. понимаю", R.drawable.w);
+            insertFood(database, "Desserts", "Pasta", "1. улицы;2. тупо;3. нигеру",
+                    "1. вырвали;2. сердце;3. Около", R.drawable.x);
+            insertFood(database, "Desserts", "Pizza", "1. часа;2. вдыхаю;3. дым",
+                    "1. в;2. эту;3. бездну", R.drawable.y);
+            insertFood(database, "Desserts", "Soup", "1. бесталанный;2. soundboy;3. DJ",
+                    "1. Будто;2. рукав;3. не пришей", R.drawable.z);
         }
 
         if (oldVersion < 2) {
