1649161dca41092dc3d58621bca6e9768eb79548
Kirill K <dev.kirill.klimenkov@gmail.com>
2018-12-16 22:49:40 +0300

    Fix read info from db

--- app/src/main/java/com/example/kirill/cookbook/CategoriesFragment.java
+++ app/src/main/java/com/example/kirill/cookbook/CategoriesFragment.java
@@ -7,7 +7,9 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,25 +28,27 @@ public class CategoriesFragment extends Fragment {
     public final String[] DatabaseCategories= {"Snacks", "Salads", "Main", "Desserts"};
 
     public static final int gridColumnCount = 2;
-    public int[][] categoriesIds;
+    public int[][] categoriesIds = new int[DatabaseCategories.length][gridColumnCount];
     public int randomId;
     private final Random random = new Random();
 
-    public int[][] images;
-    public String[][] names;
+    public int[][] images = new int[DatabaseCategories.length][gridColumnCount];;
+    public String[][] names = new String[DatabaseCategories.length][gridColumnCount];
+    public boolean wasCreated = false;
 
     public CategoriesFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         if (savedInstanceState != null) {
+            wasCreated = savedInstanceState.getBoolean("Created");
+            Log.i("ON CREATE   ::::      ", Boolean.toString(wasCreated));
+//            setRetainInstance(true);
+
             names[0] = savedInstanceState.getStringArray("SnackNames");
             names[1] = savedInstanceState.getStringArray("SaladNames");
             names[2] = savedInstanceState.getStringArray("MainNames");
@@ -55,31 +59,40 @@ public class CategoriesFragment extends Fragment {
             images[2] = savedInstanceState.getIntArray("MainImages");
             images[3] = savedInstanceState.getIntArray("DessertImages");
         }
+    }
 
-//        categoriesIds = new int[DatabaseCategories.length][gridColumnCount];
-//        images = new int[DatabaseCategories.length][gridColumnCount];
-//        names = new String[DatabaseCategories.length][gridColumnCount];
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
 
-        // Random choice of food from BD by CATEGORY
-        FoodDatabaseHelper foodDatabaseHelper = new FoodDatabaseHelper(inflater.getContext());
-        try {
-            SQLiteDatabase database = foodDatabaseHelper.getReadableDatabase();
+        onCreate(savedInstanceState);
 
-            for (int rowGrid = 0; rowGrid < DatabaseCategories.length; rowGrid++) {
-                for (int columnGrid = 0; columnGrid < gridColumnCount; columnGrid++) {
-                    Cursor cursor = getRandomCategoryRecordFromDb(database, DatabaseCategories[rowGrid]);
+        if (!wasCreated) {
 
-                    // get all needed information about food
-                    if (cursor.moveToFirst()) {
-                        categoriesIds[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("_id"));
-                        images[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("IMAGE_RESOURCE_ID"));
-                        names[rowGrid][columnGrid] = cursor.getString(cursor.getColumnIndex("NAME"));
+            // Random choice of food from BD by CATEGORY
+            FoodDatabaseHelper foodDatabaseHelper = new FoodDatabaseHelper(inflater.getContext());
+            try {
+                SQLiteDatabase database = foodDatabaseHelper.getReadableDatabase();
+
+                for (int rowGrid = 0; rowGrid < DatabaseCategories.length; rowGrid++) {
+                    for (int columnGrid = 0; columnGrid < gridColumnCount; columnGrid++) {
+                        Cursor cursor = getRandomCategoryRecordFromDb(database, DatabaseCategories[rowGrid]);
+
+                        // get all needed information about food
+                        if (cursor.moveToFirst()) {
+                            categoriesIds[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("_id"));
+                            images[rowGrid][columnGrid] = cursor.getInt(cursor.getColumnIndex("IMAGE_RESOURCE_ID"));
+                            names[rowGrid][columnGrid] = cursor.getString(cursor.getColumnIndex("NAME"));
+                        }
                     }
                 }
-            }
+                wasCreated = true;
 
-        } catch (SQLException e) {
-            Toast.makeText(inflater.getContext(), "Database unavailable", Toast.LENGTH_SHORT).show();
+            } catch (SQLException e) {
+                Toast.makeText(inflater.getContext(), "Database unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
 
 
@@ -154,6 +167,9 @@ public class CategoriesFragment extends Fragment {
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.i("HELLO : ", "OnSAVE");
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("Created", wasCreated);
         outState.putStringArray("SnackNames", names[0]);
         outState.putStringArray("SaladNames", names[1]);
         outState.putStringArray("MainNames", names[2]);
@@ -163,5 +179,10 @@ public class CategoriesFragment extends Fragment {
         outState.putIntArray("SaladImages", images[1]);
         outState.putIntArray("MainImages", images[2]);
         outState.putIntArray("DessertImages", images[3]);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 }
