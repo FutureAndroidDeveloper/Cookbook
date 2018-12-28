ac513a92750deabb91a5509c58079ce7cda25cf5
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-12-28 19:24:57 +0300

    Added action to share the application

--- app/src/main/java/com/example/kirill/cookbook/MainActivity.java
+++ app/src/main/java/com/example/kirill/cookbook/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.kirill.cookbook;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ViewPager pager;
     private TabLayout tabLayout;
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_item, menu);
 
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent(getString(R.string.share_text));
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -116,6 +125,21 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setShareActionIntent(String text) {
+//        Uri image = Uri.parse("android.resource://" + getPackageName()
+//                + "/drawable/" + "a");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+//
+//        intent.setAction(Intent.ACTION_SEND);
+//        intent.putExtra(Intent.EXTRA_TEXT, "Text");
+//        intent.putExtra(Intent.EXTRA_STREAM, image);
+//        intent.setType("image/png");
+//        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 }
 
